ae5fcf51f48435bae42bc714d0aa0a7e2808e16a
theNgoc <thengoc@imgroup.vn>
2019-01-05 23:23:15 +0700

    fix bug sql

--- API/process/bookProcess.js
+++ API/process/bookProcess.js
@@ -98,8 +98,8 @@ LEFT JOIN user ON user.id = BookCar.biker where customer.phone = '${phone_custom
 
 exports.changeStatus = function(phone_of_customer, time_sent_request, status_wish_change) {
 	var sql = `UPDATE BookCar 
-SET status LIKE '${status_wish_change}' 
-WHERE customer IN (SELECT c.id FROM customer c WHERE c.phone LIKE '${phone_of_customer}')
+SET status = '${status_wish_change}' 
+WHERE customer IN (SELECT c.id FROM customer c WHERE c.phone LIKE '${phone_of_customer}') 
 AND time like '${time_sent_request}';`;
 	return db.write(sql);
 }
